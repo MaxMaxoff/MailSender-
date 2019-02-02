822b7f08d75f5f3da3ff0c33f7f9a25c66efd960
Maxim Toropov <maxim.vt@gmail.com>
2019-02-02 16:13:15 +0300

    update Home Work 1

--- WpfTestMailSender/MainWindow.xaml.cs
+++ WpfTestMailSender/MainWindow.xaml.cs
@@ -26,11 +26,11 @@ namespace WpfTestMailSender
 
             using (MailMessage message = new MailMessage())
             {
-                message.From = new MailAddress(messageForm.mailFrom, messageForm.messageFrom);
-                message.To.Add(messageForm.messageTo);
-                message.Subject = messageForm.messageSubject;
-                message.Body = messageForm.messageBody;
-                message.IsBodyHtml = messageForm.isMessageBodyHTML;
+                message.From = new MailAddress(messageForm.MailFrom, messageForm.MessageFrom);
+                message.To.Add(messageForm.MessageTo);
+                message.Subject = messageForm.MessageSubject;
+                message.Body = messageForm.MessageBody;
+                message.IsBodyHtml = messageForm.IsMessageBodyHTML;
 
                 try
                 {
--- WpfTestMailSender/Settings/ServerSettings.cs
+++ WpfTestMailSender/Settings/ServerSettings.cs
@@ -8,9 +8,26 @@ namespace WpfTestMailSender
 {
     public class ServerSettings
     {
-        public static string serverAddress = "smtp.yandex.ru";
+        private static string serverAddress; // = "smtp.yandex.ru";
+        private static int serverPort; // = 25;
 
-        public static int serverPort = 25;
+        public string ServerAddress
+        {
+            get { return serverAddress; }
+            set { serverAddress = value; }
+        }
+
+        public int ServerPort
+        {
+            get { return serverPort; }
+            set { serverPort = value; }
+        }
+
+        public ServerSettings()
+        {
+            ServerAddress = "smtp.yandex.ru";
+            ServerPort = 25;
+        }
 
     }
 }
