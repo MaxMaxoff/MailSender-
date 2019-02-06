7705530cdc8b02118e84193e59234808ae19e007
Maxim Toropov <maxim.vt@gmail.com>
2019-02-06 21:10:51 +0300

    Очистка решения.

---
+++ MailSender.lib/Data/Debug/SchedulerTasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Data.Debug
+{
+    public static class SchedulerTasks
+    {
+        public static List<SchedulerTask> Items { get; } = new List<SchedulerTask>
+        {
+            new SchedulerTask{Id = 1, Name = "", Time = DateTime.Now.AddHours(1), Mail = "Mail 1"},
+            new SchedulerTask{Id = 2, Name = "", Time = DateTime.Now.AddHours(2), Mail = "Mail 2"},
+            new SchedulerTask{Id = 3, Name = "", Time = DateTime.Now.AddHours(3), Mail = "Mail 3"},
+        };
+    }
+
+    public class SchedulerTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public string Mail { get; set; }
+    }
+}
---
+++ MailSender.lib/Data/Debug/Senders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Data.Debug
+{
+    public static class Senders
+    {
+        public static List<Sender> Items { get; } = new List<Sender>
+        {
+            new Sender{Id = 1, Name = "Иванов", Address = "ivanov@yandex.ru"},
+            new Sender{Id = 2, Name = "Петров", Address = "petrov@yandex.ru"},
+            new Sender{Id = 3, Name = "Сидоров", Address = "sidorov@yandex.ru"},
+        };
+    }
+
+    public class Sender
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
