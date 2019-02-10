41c62e8efc5ef9123767cd5e715154b4c937e4c3
Maxim VT <maxim.vt@gmail.com>
2019-02-10 13:12:05 +0300

    CleanUp

--- MailSender/ViewModel/ViewModelLocator.cs
+++ MailSender/ViewModel/ViewModelLocator.cs
@@ -30,9 +30,15 @@ namespace MailSender.ViewModel
             
             // InMemory for Debug
             SimpleIoc.Default.Register<IRecipientsData, InMemoryRecipientsData>();
+            SimpleIoc.Default.Register<IServersData, InMemoryServersData>();
+            SimpleIoc.Default.Register<ISendersData, InMemorySendersData>();
+            SimpleIoc.Default.Register<ISchedulerTasksData, InMemorySchedulerTasksData>();
             
             // InLinq2SQL for real data
-            // SimpleIoc.Default.Register<IRecipientsData, InLinq2SQLRecipientsData>();
+            //SimpleIoc.Default.Register<IRecipientsData, InLinq2SQLRecipientsData>();
+            //SimpleIoc.Default.Register<IServersData, InLinq2SQLServersData>();
+            //SimpleIoc.Default.Register<ISendersData, InLinq2SQLSendersData>();
+            //SimpleIoc.Default.Register<ISchedulerTasksData, InLinq2SQLSchedulerTasksData>();
             
             if (!SimpleIoc.Default.IsRegistered<MailDataBaseContext>())
                 SimpleIoc.Default.Register(() => new MailDataBaseContext());
